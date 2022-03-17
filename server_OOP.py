71d46c56903c87034a5935e56f67e2e8315c5cd8
Skatch <101039462+SkatchUP@users.noreply.github.com>
2022-03-17 13:39:31 +0300

    Update server_OOP.py

--- server_OOP.py
+++ server_OOP.py
@@ -3,6 +3,7 @@ import threading
 
 import requests
 from flask import Flask, request, Response, redirect, render_template, session
+import Functionals
 from datetime import datetime
 app = Flask(__name__)
 
@@ -17,9 +18,12 @@ def start():
 def register():
     login = request.json['login']
     password = request.json['password']
-    return_password = request.json['return_password']
-    full_info = {'login': login, 'password': password, 'return_password': return_password}
-    if full_info['login'] == '' or full_info['password'] == '' or full_info['return_password'] == '':
+    chek_password = request.json['return_password']
+    if password == chek_password:
+        full_info = {'login': login, 'password': password, 'chek_password': chek_password}
+    else:
+        return Response(status=228)
+    if full_info['login'] == '' or full_info['password'] == '' or full_info['chek_password'] == '':
         return Response(status=400)
     else:
         users.append(full_info)
